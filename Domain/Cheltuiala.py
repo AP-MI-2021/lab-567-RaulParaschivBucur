d6a1861259c6cfa74d450f0ca78f57a272a590c7
RaulParaschivBucur <91596727+RaulParaschivBucur@users.noreply.github.com>
2021-10-22 18:55:22 +0300

    .

---
+++ Domain/Cheltuiala.py
@@ -0,0 +1,57 @@
+def creeaza_cheltuiala(nr_apartament: int, suma, data, tip_cheltuiala):
+    """
+    :param nr_apartament: numarul apartamentului, trebuie sa fie unic
+    :param suma: suma cheltuita, nenula
+    :param data: data cheltuielii
+    :param tip_cheltuiala: tipul cheltuielii
+    :return: o cheltuiala
+    """
+    return {
+        'nr_ap': nr_apartament,
+        'suma': suma,
+        'data': data,
+        'tip_chelt': tip_cheltuiala
+    }
+
+
+def get_nr_apartament(Cheltuiala):
+    """
+    Getter pt numarul apartamentului
+    :param Cheltuiala: o cheltuiala
+    :return: numarul apartamentului aferent cheltuielii date ca parametru
+    """
+    return Cheltuiala['nr_ap']
+
+
+def get_suma(Cheltuiala):
+    """
+    Getter pt suma cheltuita
+    :param Cheltuiala: o cheltuiala
+    :return: suma cheltuita
+    """
+    return Cheltuiala['suma']
+
+
+def get_data(Cheltuiala):
+    """
+    Getter pt data cheltuielii
+    :param Cheltuiala: o cheltuiala
+    :return: data cheltuielii date ca parametru
+    """
+    return Cheltuiala['data']
+
+
+def get_tip_cheltuiala(Cheltuiala):
+    """
+    Getter pt tipul cheltuielii
+    :param Cheltuiala: o cheltuiala
+    :return: tipul cheltuielli date ca parametru
+    """
+    return Cheltuiala['tip_chelt']
+
+
+def get_str(Cheltuiala):
+    """
+    Getter pt mesajul cu continutul cheltuielii
+    """
+    return f'Cheltuiala apartament nr: {get_nr_apartament(Cheltuiala)}, cu suma cheltuita: {get_suma(Cheltuiala)}, in data: {get_data(Cheltuiala)}. Tipul cheltuielii: {get_tip_cheltuiala(Cheltuiala)}.'
---
+++ Logic/CRUD.py
@@ -0,0 +1,65 @@
+from Domain.Cheltuiala import creeaza_cheltuiala, get_nr_apartament
+
+
+def create(lst_cheltuieli, nr_apartament: int, suma, data, tip_cheltuiala):
+    """
+    Creaza o noua cheltuiala
+    :param nr_apartament: numarul apartamentului, trebuie sa fie unic
+    :param suma: suma cheltuita, nenula
+    :param data: data cheltuielii
+    :param tip_cheltuiala: tipul cheltuielii
+    :return: o cheltuiala formata din lst_cheltuieli la care se adauga o noua cheltuiala
+    """
+    cheltuiala = creeaza_cheltuiala(nr_apartament, suma, data, tip_cheltuiala)
+
+    return lst_cheltuieli + [cheltuiala]
+
+
+def read(lst_cheltuieli, nr_apartament: int=None):
+    """
+    Citeste o cheltuiala din lista de cheltuieli
+    :param lst_cheltuieli: lista de cheltuieli
+    :param nr_apartament: nr apartamentului dorit
+    :return: cheltuiala apartamentului cu numarul nr_apartament sau lista cu toate cheltuielile daca nr_apartament=None
+    """
+    cheltuiala_ap_cu_nr = None
+    for cheltuiala in lst_cheltuieli:
+        if get_nr_apartament(cheltuiala) == nr_apartament:
+            cheltuiala_ap_cu_nr = cheltuiala
+
+    if cheltuiala_ap_cu_nr == None:
+        return lst_cheltuieli
+    else:
+        return cheltuiala_ap_cu_nr
+
+
+def update(lst_cheltuieli, new_cheltuiala):
+    """
+    Modifica o cheltuiala din lista de cheltuieli
+    :param lst_cheltuieli: lista de cheltuieli
+    :param nr_apartament: cheltuiala care se va amodifica, nr apartamentului trebuie sa fie unul existent
+    :return: o lista cu cheltuiala actualizata
+    """
+    new_lst_cheltuieli = []
+    for cheltuiala in lst_cheltuieli:
+        if get_nr_apartament(cheltuiala) != get_nr_apartament(new_cheltuiala):
+            new_lst_cheltuieli.append(cheltuiala)
+        else:
+            new_lst_cheltuieli.append(new_cheltuiala)
+
+    return new_lst_cheltuieli
+
+
+def delete(lst_cheltuieli, nr_apartament: int):
+    """
+    Sterge o cheltuiala din lista de cheltuieli
+    :param lst_cheltuieli: lista da cheltuieli
+    :param nr_apartament: nr apartamentului pe care vrem sa il stergem
+    :return: lista lst_cheltuieli fara cheltuiala cu numarul nr_apartament
+    """
+    result_lst = []
+    for cheltuiala in lst_cheltuieli:
+        if get_nr_apartament(cheltuiala) != nr_apartament:
+            result_lst.append(cheltuiala)
+
+    return result_lst
