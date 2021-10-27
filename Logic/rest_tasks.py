b63a9a6040ace6ccd3da418f5d11d812b9389bc9
RaulParaschivBucur <91596727+RaulParaschivBucur@users.noreply.github.com>
2021-10-27 13:05:29 +0300

    Lab567

--- Logic/rest_tasks.py
+++ Logic/rest_tasks.py
@@ -4,7 +4,7 @@ from Domain.Cheltuiala2 import get_nr_apartament
 
 def stergere_toate_chelt(lst_cheltuieli, nr_apartament: int):
     """
-    Sterge toate cheltuielile pt un anumi apartament
+    Sterge toate cheltuielile pt un anumit apartament
     :param lst_cheltuieli: lista de cheltuieli
     :param nr_apartament: numarul apartamentului caruia vrem sa ii stergem toate cheltuielile
     :return: lista lst_cheltuieli fara cheltuielile cu numarul de apartament nr_apartament
--- Tests/test_rest_tasks.py
+++ Tests/test_rest_tasks.py
@@ -1,15 +1,19 @@
+from Domain.Cheltuiala2 import get_nr_apartament
 from Logic.CRUD import read
 from Logic.rest_tasks import stergere_toate_chelt
 from Tests.test_CRUD import get_dat3
 
 
+# necesita modificari
 def test_sterge_toate_chelt():
     lst_cheltuieli = get_dat3()
-    nr_chelt_de_sters = 4
-    chelt_de_sters = read(lst_cheltuieli, nr_chelt_de_sters)
-    deleted = stergere_toate_chelt(lst_cheltuieli, nr_chelt_de_sters)
-
-    assert chelt_de_sters not in deleted
+    nr_ap_chelt_de_sters = 4
+    deleted = stergere_toate_chelt(lst_cheltuieli, nr_ap_chelt_de_sters)
+    ok = True
+    for cheltuiala in deleted:
+        if get_nr_apartament(cheltuiala) == nr_ap_chelt_de_sters:
+            ok = False
+    assert ok
 
 
 def test_rest_tasks():
